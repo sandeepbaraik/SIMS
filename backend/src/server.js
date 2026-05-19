a19071de2018f63113519cd57e825be4005fe17a
Sandeep Baraik <53049634+sandeepbaraik@users.noreply.github.com>
2026-05-19 18:18:20 +0530

    updated cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,7 @@ const { sequelize } = require("./models");
 const app = express();
 const port = Number(process.env.PORT || 5000);
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
